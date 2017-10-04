37458a1231e02f9b4d71b12d005440e3939a26f1
jonkeller <kellejm2@gmail.com>
2017-10-03 21:23:02 -0400

    Adds simple auto control of sail sheet, not rudder

--- RoboSail.h
+++ RoboSail.h
@@ -30,3 +30,43 @@ int sailPosition = 45;
 Servo rudderServo;
 Servo sailServo;
 
+// Compass (magnetometer and accelerometer variables
+// These values will need to be adjusted based on your particular compass.
+// Use compassCalibration (in the Orientation library) to determine the correct hard iron calibration.
+float hardiron_x = -11.91;
+float hardiron_y = 5.05;
+float hardiron_z = -1.02;
+
+float pitchAccel = 0; //raw accelerometer value
+float rollAccel = 0;  //raw accelerometer value
+float yawAccel = 0;   //raw accelerometer value
+float robosailRollAccel = 0;  // converted so Port lean is positive
+float pitch = 0;  //value in degrees, converted from raw data
+float roll = 0;   //value in degrees, converted from raw data
+float yaw = 0;    //value in degrees, converted from raw data
+
+float heading;  //value in degrees
+float robosailHeading;  //value in degrees, converted to RoboSail FrameofRef
+float robosailRoll;   // converted so Port lean is positive
+
+// Source: http://www.ngdc.noaa.gov/geomag-web/#igrfwmm
+float declination = -14.6067;
+/* Assign a ID to Accelerometer and Magnetometer and define eventt */
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+sensors_event_t event;
+
+//variables for GPS
+// this will be false until GPS fix is found and starting position saved
+bool start_pos_found = false;
+int GPSfix;
+int GPSqual;
+int GPSsat;
+// once GPS fix is found, these variables will be updated
+float startPositionX = 0;
+float startPositionY = 0;
+float relPositionX = 0;
+float relPositionY = 0;
+float XYpos[2]; // this is xy position - it is in a 2 place array for x, y
+float angleFromStart = 0;
+
