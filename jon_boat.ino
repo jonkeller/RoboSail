f15b87457002330fbad43eab38e2d63d871498a9
jonkeller <kellejm2@gmail.com>
2017-10-07 10:11:01 -0400

    Partially implements rudder control

--- jon_boat.ino
+++ jon_boat.ino
@@ -17,28 +17,46 @@ Adafruit_GPS GPS(&Serial);
 // initialize utility that will convert lat/lon to (x,y) positions in meters
 UsefulCalcs calc(false);
 
+//                       Middle      Dock
+float target_lats[3] = { 42.360601,  42.360389};
+float target_lons[3] = {-71.073761, -71.073364};
+
+float target_lat;
+float target_lon;
+int IRONS_DEG = 45;
+
 void setup() {
   Serial.begin(115200);
+  Serial.println("In setup()");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   accel.begin();
   mag.begin();
 
-  Serial.println("\nRoboSail BoatCode - XXXXX\n");  //write program name here
+  Serial.println("\nRoboSail BoatCode - 0.02\n");  //write program name here
   // Set RC receiver and WindSensor on digital input pins
   declarePins();
 
+  Serial.println("Setting up GPS");
   checkGPS();
+  Serial.println("Setting up Compass");
   checkCompass();
+  Serial.println("Ready to loop");
+  digitalWrite(LED_BUILTIN, LOW);
+  test_find_absolute_angle(); // TODO Remove
 }
 
 void loop() {
+  return; // TODO Remove
+  Serial.println("I am in loop");
   //*********** Read in data from the RC receiver and sensors *********
   readReceiver();
   readWind();
-  readAccel();
-  readGPS();  //puts values in "start" and "relative" variable
+  //readAccel();
+  //readGPS();  //puts values in "start" and "relative" variable
 
   //Read heading and tilt from the Compass
-  readCompassAccel();
+  //readCompassAccel();
 
   // You now have values from the RC Receiver and Sensors in these variables: 
   // rudderPosition, sailPosition, and windAngle, 
@@ -54,20 +72,17 @@ void loop() {
   
   Serial.print("Sail position: ");
   Serial.println(sailPosition);
-  int sailPct = map(constrain(sailPosition, SAIL_LOW, SAIL_HIGH), SAIL_LOW, SAIL_HIGH, 0, 100);
+  int sailPct = map(constrain(sailPosition, 0, 90), 0, 90, 0, 100);
   Serial.print("Sail position %:");
   Serial.println(sailPct);
   
   if (sailPct >= 90) { // Autopilot mode iff sail joystick is at/above 90% 
     Serial.println("In autopilot mode");
-    // abs(windAngle): 45...180 -> abs(sail) 0...90
-    sailPosition = map(constrain(abs(windAngle), 45, 180), 45, 180, 0, 90);
-    Serial.print("Autopilot sail position:");
-    Serial.println(sailPosition);
   
-    //rudderPosition = TODO: Use rudder to follow planned path
-    // TODO: Goal: 42.360685, -71.073914
     // Started at startPositionX, startPositionY iff start_pos_found is true
+    choose_target();
+    set_rudder();
+    set_sail();
   } else {
     Serial.println("In manual mode");
   }
@@ -80,4 +95,92 @@ void loop() {
   
 } //end of loop()
 
+void choose_target() {
+  // TODO: Keep track of which target is next, whether to approach it on the left or right, and by how wide a margin
+  target_lat = target_lats[0];
+  target_lon = target_lons[0];
+}
+
+void set_rudder() {
+  // Point at the target, unless that would put us in irons,
+  // in which case go at a IRONS_DEG-degree angle toward-ish it
+  float absolute_angle = find_absolute_angle(GPS.latitudeDegrees, GPS.longitudeDegrees, target_lat, target_lon);
+  Serial.print("Want to head at angle: "); Serial.println(absolute_angle);
+  Serial.print("Current heading: "); Serial.println(robosailHeading);
+
+  float bearing = absolute_angle - robosailHeading;
+  Serial.print("Desired bearing: "); Serial.println(bearing);
+
+  float bearing_relative_to_wind = windAngle - bearing;
+  Serial.print("Wind angle: "); Serial.println(windAngle);
+  Serial.print("Bearing relative to wind: "); Serial.println(bearing_relative_to_wind);
+  if (abs(bearing_relative_to_wind) <= IRONS_DEG) {
+    rudderPosition = bearing_relative_to_wind > 0 ? bearing_relative_to_wind+IRONS_DEG : bearing_relative_to_wind-IRONS_DEG;
+    rudderPosition = constrain(rudderPosition, -60, 60);
+    Serial.print("Rudder position, to avoid irons: "); Serial.println(rudderPosition);
+  } else {
+    rudderPosition = constrain(bearing, -60, 60);
+    Serial.print("Rudder position: "); Serial.println(rudderPosition);
+  }
+}
+
+// Note: does not do great-circle distance, so not suitable for
+// distances >= hundreds of km. But works fine for things on the
+// order of tens of meters.
+// This gives the graph-paper angle, i.e. 0 = to the right, 90 = up
+float find_absolute_angle(float from_lat, float from_lon, float to_lat, float to_lon) {
+  float lat_delta = to_lat - from_lat;
+  float lon_delta = to_lon - from_lon;
+  float angle;
+  if (abs(lon_delta) <= 0.0001) {
+    angle = lon_delta > 0 ? 0 : 180;
+  } else {
+    angle = atan2(lat_delta, lon_delta);    
+  }
+  angle = angle * 57296 / 1000; // Radians to degrees
+  return angle;
+}
+
+void set_sail() {
+  // abs(windAngle): IRONS_DEG...180 -> abs(sail) 0...90
+  sailPosition = map(constrain(abs(windAngle), IRONS_DEG, 180), IRONS_DEG, 180, 0, 90);
+  Serial.print("Autopilot sail position:");
+  Serial.println(sailPosition);
+  // TODO Pull it in a little and then let it out when gybing
+}
+
+void test_find_absolute_angle() {
+  float angle;
+  angle = find_absolute_angle(0, 0, 100, 0);
+  Serial.print("North: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(0, 0, 100, 100);
+  Serial.print("Northwest: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(10, 10, 10, 20);
+  Serial.print("West: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(10, 10, 0, 20);
+  Serial.print("Southwest: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(39, -86, 18, -86);
+  Serial.print("South: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(39, -86, 19, -66);
+  Serial.print("Southeast: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(39, -86, 39, 5);
+  Serial.print("East: ");
+  Serial.println(angle);
+
+  angle = find_absolute_angle(39, -86, 40, -85);
+  Serial.print("Northeast: ");
+  Serial.println(angle);  
+}
 
